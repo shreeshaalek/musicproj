8c581fdc1870c5d81a32f2a10d78ba539a5e7c22
shreesha <shreesha.alek@gmail.com>
2017-11-05 21:44:47 +0530

    Fixed client side google login

--- codeBase/server.js
+++ codeBase/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser')
 
-// var GoogleAuth = require('google-auth-library');
+var GoogleAuth = require('google-auth-library');
 
 
 let app = express();
@@ -18,24 +18,25 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './', 'dist', 'index.html'));
 });
 
-var auth = new GoogleAuth;
-var client = new auth.OAuth2(CLIENT_ID, '', '');
-client.verifyIdToken(
-    token,
-    CLIENT_ID,
-    // Or, if multiple clients access the backend:
-    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
-    function(e, login) {
-      var payload = login.getPayload();
-      var userid = payload['sub'];
-      // If request specified a G Suite domain:
-      //var domain = payload['hd'];
-    });
-
+// var auth = new GoogleAuth;
+// var client = new auth.OAuth2(CLIENT_ID, '', '');
+// client.verifyIdToken(
+//     token,
+//     CLIENT_ID,
+//     // Or, if multiple clients access the backend:
+//     //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
+//     function(e, login) {
+//       var payload = login.getPayload();
+//       var userid = payload['sub'];
+//       // If request specified a G Suite domain:
+//       //var domain = payload['hd'];
+//     });
+app.get('/tokensignin', (req, res) => {
+  res.send('hello')
+});
 app.post('/tokensignin', (req, res) => {
-  console.log("insiden server")
-    console.log(JSON.stringify(req.body))
+    res.send('hello')
     // res.send('hello'); 
   });
 
-app.listen(8080, ()=>{console.log('listening in port 8080');});
+app.listen(3000, ()=>{console.log('listening in port 3000');});
